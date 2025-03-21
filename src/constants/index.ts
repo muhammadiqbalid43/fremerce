adbc4c5e6647f795577656b3130009cf56eb4d16
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-21 09:14:17 +0700

    feat: add dummy testimonials data

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    name: "Muhammad Iqbal",
+    location: "New York, USA",
+    rating: 5,
+    text: "I absolutely love the quality of the clothes! The fabric is soft, durable, and the fit is perfect. Will definitely be ordering more.",
+    avatar:
+      "https://media.licdn.com/dms/image/v2/D5603AQGhS5yV_c04vg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725841510191?e=1747872000&v=beta&t=kLNqpJNe0gzloBKUZJETFVTq67vd7uIwxqOW27x1ThE",
+  },
+  {
+    name: "Fadli Hidayatullah",
+    location: "London, UK",
+    rating: 5,
+    text: "Fast shipping and excellent customer service. The attention to detail in packaging was impressive. My new favorite online store!",
+    avatar:
+      "https://media.licdn.com/dms/image/v2/D5603AQGxsL9X0inFLQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1684914683163?e=1747872000&v=beta&t=1PDQtoI-AQN_8yiGkw7-mJw9-2QcPUldzp69Gdy5zNY",
+  },
+  {
+    name: "Fatimah az-zahra",
+    location: "Sydney, Australia",
+    rating: 4,
+    text: "Great selection of trendy items at reasonable prices. The return process was smooth when I needed to exchange a size.",
+    avatar:
+      "https://fyf.tac-cdn.net/images/products/small/FYF-118.jpg?auto=webp&quality=60&width=650",
+  },
+];
